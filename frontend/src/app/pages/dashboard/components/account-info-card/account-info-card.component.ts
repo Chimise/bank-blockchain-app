4ac44781b1b60afc662ea39360d99d1c583c2aa5
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-02-29 18:22:30 +0100

    content of the page complete, only styling left

--- frontend/src/app/pages/dashboard/components/account-info-card/account-info-card.component.ts
+++ frontend/src/app/pages/dashboard/components/account-info-card/account-info-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account-info-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './account-info-card.component.html',
   styleUrl: './account-info-card.component.css'
 })
@@ -11,4 +12,8 @@ export class AccountInfoCardComponent {
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() src: string = '';
+  @Input() svgBgColor: string = '';
+  @Input() hoverBorderColor: string = '';
+  @Input() hoverShadowColor: string = '';
+  @Input() hover = false;
 }
--- frontend/src/app/pages/dashboard/components/transaction-history/transaction-history.component.ts
+++ frontend/src/app/pages/dashboard/components/transaction-history/transaction-history.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Transaction {
+  id: string;
+  accountName: string;
+  date: string;
+  type: string;
+  amount: number; // Positive for credit, negative for debit
+}
 
 @Component({
   selector: 'app-transaction-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.css'
 })
-export class TransactionHistoryComponent {
+export class TransactionHistoryComponent implements OnInit{
+  transactions: Transaction[] = [
+    { id: 'TX123', accountName: 'Savings Account', date: '2024-02-28', type: 'Satellite Bills', amount: -150.00 },
+    { id: 'TX124', accountName: 'Checking Account', date: '2024-02-27', type: 'Shopping Service', amount: 200.00 },
+    { id: 'TX125', accountName: 'Savings Account', date: '2024-02-26', type: 'Grocery', amount: -50.00 }
+  ];
+
+  i = 1;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
