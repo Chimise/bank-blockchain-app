dcb6f18d941ccabe1787a66e2bf34f3687d5c1f4
chisom promise <chimisepro@gmail.com>
2024-03-01 09:28:08 +0100

    feat: create common utils

--- backend/src/main/java/com/firstacademy/firstblock/util/SecretKeyUtils.java
+++ backend/src/main/java/com/firstacademy/firstblock/util/SecretKeyUtils.java
@@ -1,14 +1,12 @@
 package com.firstacademy.firstblock.util;
 
-import java.nio.charset.StandardCharsets;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 public class SecretKeyUtils {
     public static SecretKey generateSecretKey(String secretKey) {
-        byte[] decodedKey = secretKey.getBytes(StandardCharsets.UTF_8);
-        SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA512");
-        return key;
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 }
