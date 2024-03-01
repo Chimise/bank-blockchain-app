cf0279ac3a6edddfe385e0abc8dec1fed136dd06
chisom promise <chimisepro@gmail.com>
2024-03-01 09:30:17 +0100

    feat: create dummy users

--- backend/src/main/java/com/firstacademy/firstblock/FirstblockApplication.java
+++ backend/src/main/java/com/firstacademy/firstblock/FirstblockApplication.java
@@ -1,13 +1,19 @@
 package com.firstacademy.firstblock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.firstacademy.firstblock.repository.RoleRepository;
 import com.firstacademy.firstblock.repository.UserRepository;
+import com.firstacademy.firstblock.model.Role;
 import com.firstacademy.firstblock.model.User;
+import com.firstacademy.firstblock.model.UserRoles;
 
 @SpringBootApplication
 public class FirstblockApplication {
@@ -17,13 +23,21 @@ public class FirstblockApplication {
 	}
 
 	@Bean
-	public CommandLineRunner dataLoader(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+	public CommandLineRunner dataLoader(UserRepository userRepo, RoleRepository roleRepo,
+			PasswordEncoder passwordEncoder) {
 		return args -> {
+			roleRepo.deleteAll();
+
+			Role admin = roleRepo.save(new Role().setRole(UserRoles.ADMIN));
+			Role user = roleRepo.save(new Role().setRole(UserRoles.USER));
+
+			userRepo.deleteAll();
 			userRepo.save(new User().setEmail("chimisepro@gmail.com").setPassword(passwordEncoder.encode("password123"))
+					.setRoles(Arrays.asList(admin))
 					.setFirstName("Chisom").setLastName("Promise"));
 			userRepo.save(new User().setEmail("prosom@gmail.com").setPassword(passwordEncoder.encode("password$123"))
+					.setRoles(Arrays.asList(user))
 					.setFirstName("Nasiru").setLastName("Ibraham"));
 		};
 	}
-
 }
--- backend/src/main/java/com/firstacademy/firstblock/repository/RoleRepository.java
+++ backend/src/main/java/com/firstacademy/firstblock/repository/RoleRepository.java
@@ -1,6 +1,8 @@
 
 package com.firstacademy.firstblock.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.firstacademy.firstblock.model.Role;
