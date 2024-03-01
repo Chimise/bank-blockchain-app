e0818d502e6e32dc5ccb09f0a2dd94eb14469747
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-01 12:26:23 +0100

    added the backend auth logic and login page

--- frontend/src/app/pages/homepage/homepage.component.ts
+++ frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
