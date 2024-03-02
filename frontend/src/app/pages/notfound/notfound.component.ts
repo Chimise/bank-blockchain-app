1aa2756ce1d8291c34ba61844d0b1d504070b05b
Charles Lughas <lordlughas@gmail.com>
2024-03-02 08:04:40 +0100

    Add the NotFound page

--- frontend/src/app/pages/notfound/notfound.component.ts
+++ frontend/src/app/pages/notfound/notfound.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.css'
 })
