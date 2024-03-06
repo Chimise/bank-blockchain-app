1ce249c5956181ff22f63ce5c838c495286bf973
chisom promise <chimisepro@gmail.com>
2024-03-06 08:42:47 +0100

    feat: create basic controllers that enables client talk to fabric

---
+++ backend/src/main/java/com/firstacademy/firstblock/controllers/AccountController.java
@@ -0,0 +1,61 @@
+package com.firstacademy.firstblock.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstacademy.firstblock.dto.request.CreateAccountRequest;
+import com.firstacademy.firstblock.dto.response.Response;
+import com.firstacademy.firstblock.service.BlockchainService;
+import com.firstacademy.firstblock.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BlockchainService blockchainService;
+
+    @GetMapping
+    public ResponseEntity<String> getUserAccounts() throws Exception {
+        var user = userService.getCurrentUser();
+        String response = blockchainService.readUserAccounts(user.getId());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{accountNo}")
+    public ResponseEntity<String> getAccount(@PathVariable("accountNo") String accountNo) throws Exception {
+        String response = blockchainService.readAccount(accountNo);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public Response<?> createAccount(@Valid @RequestBody CreateAccountRequest request,
+            BindingResult bindingResult) throws Exception {
+
+        if (bindingResult.hasErrors()) {
+            return Response.badRequest().setErrors(bindingResult.getFieldErrors());
+        }
+
+        request.setInitialBalance(request.getInitialBalance() * 100);
+
+        String response = blockchainService.createAccount(request.getUserId(), request.getAccountNo(),
+                request.getAccountName(), request.getInitialBalance());
+
+        return Response.ok().setPayload(response);
+    }
+}
