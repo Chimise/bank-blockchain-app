55681c783ec8f08511eef68dcd4d0c2c69976b0e
chisom promise <chimisepro@gmail.com>
2024-03-06 08:38:09 +0100

    feat: update blockchain service to update return type

--- backend/src/main/java/com/firstacademy/firstblock/service/BlockchainService.java
+++ backend/src/main/java/com/firstacademy/firstblock/service/BlockchainService.java
@@ -1,5 +1,7 @@
 package com.firstacademy.firstblock.service;
 
+import com.firstacademy.firstblock.dto.model.AccountDto;
+
 public interface BlockchainService {
 
     String invokeQuery(String functionName, String... args) throws Exception;
@@ -8,20 +10,21 @@ public interface BlockchainService {
 
     String initLedger() throws Exception;
 
-    void createAccount(int userId, String accNo, String accountName, int initialBalance) throws Exception;
+    AccountDto createAccount(Long userId, String accNo, String accountName, int initialBalance) throws Exception;
 
     boolean accountExists(String accNo) throws Exception;
 
-    void updateAccount(int userId, String accNo, String name, String type, String status) throws Exception;
+    void updateAccount(Long userId, String accNo, String name, String type, String status) throws Exception;
 
     String readAccount(String accNo) throws Exception;
 
-    void transferFunds(int senderUserId, String transactionId, String senderAccNo, String recieverAccNo, int amount, String narration, String timestamp) throws Exception;
+    String transferFunds(Long senderUserId, String transactionId, String senderAccNo, String recieverAccNo, int amount,
+            String narration, String timestamp) throws Exception;
 
     String readTransaction(String transactionId) throws Exception;
 
     String readTransactionHistory(String accNo) throws Exception;
-    
-    String readUserAccounts(int userId) throws Exception;
+
+    String readUserAccounts(Long userId) throws Exception;
 
 }
